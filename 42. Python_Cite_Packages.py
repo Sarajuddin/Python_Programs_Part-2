14a20c0530f19d786061e8bc587f4be7ef155d6f
Sarajuddin <sarajuddinkazgar@gmail.com>
2023-02-22 14:51:48 +0530

    First Commit

---
+++ 42. Python_Cite_Packages.py
@@ -0,0 +1,5 @@
+# 42. Write a Python program to locate Python site-packages.
+
+import site; 
+
+print("Here is a list of site Packages : ", site.getsitepackages())
---
+++ 45. No_of_CPU_Used.py
@@ -0,0 +1,5 @@
+# 45. Write a Python program to find out the number of CPUs using.
+
+import multiprocessing
+
+print("The number of CPUs", multiprocessing.cpu_count())
