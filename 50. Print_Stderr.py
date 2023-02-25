c1c1f92189ac7591ac3b69b3a4c8f3a33109479f
Sarajuddin <sarajuddinkazgar@gmail.com>
2023-02-25 17:37:09 +0530

    Files added

---
+++ 50. Print_Stderr.py
@@ -0,0 +1,9 @@
+# 50. Write a Python program to print to stderr.
+
+from __future__ import print_function
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+eprint("abc", "efg", "xyz", sep="--")
